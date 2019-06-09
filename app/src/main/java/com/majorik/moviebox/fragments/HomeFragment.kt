b35bcd1e6cd808adb52e32855f3fd28ab8dda023
Rodion <majorik.tm@gmail.com>
2019-06-10 00:41:07 +0300

    Создание интерфейса главной страницы
    Создание активити, фрагмента и меню для главной страниц. Настройка стилей.

---
+++ app/src/main/java/com/majorik/moviebox/fragments/HomeFragment.kt
@@ -0,0 +1,21 @@
+package com.majorik.moviebox.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.majorik.moviebox.R
+
+class HomeFragment : Fragment() {
+    companion object {
+        fun newInstance() = HomeFragment()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+}
