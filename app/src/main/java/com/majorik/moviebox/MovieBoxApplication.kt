9b7e7cc88c77bc8aee6663e9fc4fb8eb4e9c07c6
Rodion <majorik.tm@gmail.com>
2020-02-09 02:25:50 +0300

    Disable minify, add a library for logging

--- app/src/main/java/com/majorik/moviebox/MovieBoxApplication.kt
+++ app/src/main/java/com/majorik/moviebox/MovieBoxApplication.kt
@@ -2,17 +2,23 @@ package com.majorik.moviebox
 
 import android.app.Application
 import com.majorik.moviebox.di.appComponent
+import com.orhanobut.logger.Logger
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 import org.koin.core.logger.Level
 import timber.log.Timber
+import timber.log.Timber.DebugTree
 
 class MovieBoxApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        Timber.plant(Timber.DebugTree())
+        Timber.plant(object : DebugTree() {
+            override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+                Logger.log(priority, tag, message, t)
+            }
+        })
 
         startKoin {
             androidLogger(Level.DEBUG)
