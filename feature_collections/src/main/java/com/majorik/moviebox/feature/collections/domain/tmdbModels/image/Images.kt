29d7c0802a89c8cc45bdb87dcc10711d29717aaf
Rodion <belovickiy.rodion@gmail.com>
2020-11-24 21:47:27 +0300

    Remove unused files from collections module

--- feature_collections/src/main/java/com/majorik/moviebox/feature/collections/domain/tmdbModels/image/Images.kt
+++
@@ -1,9 +0,0 @@
-package com.majorik.moviebox.feature.collections.domain.tmdbModels.image
-
-import com.squareup.moshi.Json
-
-data class Images(
-    @field:Json(name = "id") val id: Int,
-    @field:Json(name = "backdrops") val backdrops: List<ImageDetails>,
-    @field:Json(name = "posters") val posters: List<ImageDetails>
-)
